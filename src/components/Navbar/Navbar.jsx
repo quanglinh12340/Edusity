ac13af68d2f4edd7cdda0d85289fd4c3b701080d
quanglinh12340 <149453502+quanglinh12340@users.noreply.github.com>
2024-06-11 17:10:29 +0700

    Link navbar to position

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,8 @@
+import { useState, useEffect } from "react";
+import { Link } from "react-scroll";
 import classNames from "classnames/bind";
 import styles from "./Navbar.module.scss";
 import { assets } from "@/assets";
-import { useState } from "react";
-import { useEffect } from "react";
 
 const cx = classNames.bind(styles);
 
@@ -23,13 +23,47 @@ const Navbar = () => {
     <nav className={cx("container", `${sticky ? "dark-nav" : " "}`)}>
       <img src={assets.logo} className={cx("logo")} />
       <ul>
-        <li>Home</li>
-        <li>Program</li>
-        <li>About us</li>
-        <li>Campus</li>
-        <li>Testimonials</li>
         <li>
-          <button className={cx("btn")}>Contact us</button>
+          {" "}
+          <Link to="hero" smooth={true} offset={50} duration={500}>
+            Home
+          </Link>
+        </li>
+        <li>
+          {" "}
+          <Link to="program" smooth={true} offset={-260} duration={500}>
+            Program
+          </Link>
+        </li>
+        <li>
+          {" "}
+          <Link to="about" smooth={true} offset={-150} duration={500}>
+            About
+          </Link>{" "}
+          us
+        </li>
+        <li>
+          {" "}
+          <Link to="campus" smooth={true} offset={-260} duration={500}>
+            Campus
+          </Link>
+        </li>
+        <li>
+          {" "}
+          <Link to="testimonials" smooth={true} offset={-260} duration={500}>
+            Testimonials
+          </Link>
+        </li>
+        <li>
+          <Link
+            to="contact"
+            smooth={true}
+            offset={-260}
+            duration={500}
+            className={cx("btn")}
+          >
+            Contact us
+          </Link>
         </li>
       </ul>
     </nav>
