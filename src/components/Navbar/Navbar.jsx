69a2a03199b2f59b76715a4dc8ba4ae002ba18c7
quanglinh12340 <149453502+quanglinh12340@users.noreply.github.com>
2024-06-11 16:16:29 +0700

    handle event scroll

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,12 +1,26 @@
 import classNames from "classnames/bind";
 import styles from "./Navbar.module.scss";
 import { assets } from "@/assets";
+import { useState } from "react";
+import { useEffect } from "react";
 
 const cx = classNames.bind(styles);
 
 const Navbar = () => {
+  const [sticky, setSticky] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      window.scrollY > 500 ? setSticky(true) : setSticky(false);
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <nav className={cx("container")}>
+    <nav className={cx("container", `${sticky ? "dark-nav" : " "}`)}>
       <img src={assets.logo} className={cx("logo")} />
       <ul>
         <li>Home</li>
