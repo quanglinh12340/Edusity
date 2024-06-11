11d0656b9206da083d09da83cf56acdd6d639383
quanglinh12340 <149453502+quanglinh12340@users.noreply.github.com>
2024-06-11 16:19:09 +0700

    render ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
 import Navbar from "@/components/Navbar";
 import Hero from "@/components/Hero";
 import Programs from "@/components/Programs";
+import Title from "@/components/Title";
+import About from "@/components/About";
+import Campus from "@/components/Campus";
+import Testimonials from "@/components/Testimonials";
+import Contact from "@/components/Contact";
+import Footer from "@/components/Footer";
 
 const App = () => {
   return (
@@ -8,7 +14,16 @@ const App = () => {
       <Navbar />
       <Hero />
       <div className="container">
+        <Title subTitle="Our PROGRAM" title="What We Offer" />
         <Programs />
+        <About />
+        <Title subTitle="Gallery" title="Campus Photos" />
+        <Campus />
+        <Title subTitle="TESTIMONIALS" title="What Student Says" />
+        <Testimonials />
+        <Title subTitle="Contact Us" title="Get in Touch" />
+        <Contact />
+        <Footer />
       </div>
     </div>
   );
